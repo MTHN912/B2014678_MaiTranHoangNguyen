dbbf629747250d801fce5173a4cb8cf14bbad0a5
MTHN912 <mthnguyen912na@gmail.com>
2023-11-22 23:47:12 +0700

    last commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,8 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from 'node:url'
 
-import vue from '@vitejs/plugin-vue';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import {Server} from 'node:http'
 
 // https://vitejs.dev/config/
 export default defineConfig({
